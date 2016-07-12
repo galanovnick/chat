5b78e9117cbb7e4f3f5e6906363b863094b8c31d
Nikolay Galanov <galanov.nick@gmail.com>
2016-07-12 16:15:32 +0300

    Implementing user registration: added public api for users storage.

--- chat-app.js
+++ chat-app.js
@@ -1,7 +1,26 @@
-var chatApp = function(rootId) {
-
+var ChatApp = function(rootId) {
+	var components = [];
 	var _init = function() {
 		console.log("root initialized");
+
+		components["usersContainer"] = UsersContainer();
+	}
+
+	var UsersContainer = function() {
+		var storage = [];
+
+		var _add = function(user) {
+			
+		}
+
+		var _getAll = function() {
+
+		}
+
+		return {
+			"add": _add,
+			"getAll": _getAll
+		}
 	}
 
 	return {"init" : _init};
