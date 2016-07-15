655c81149466c8047f959efdedd2696d53548e9d
Nikolay Galanov <galanov.nick@gmail.com>
2016-07-15 11:35:45 +0300

    Updated tests: tests've been separated from each other.

--- test/test.js
+++ test/test.js
@@ -5,61 +5,70 @@ var UserStorage = require('../scripts/userStorage');
 var UserService = require('../scripts/userService');
 var UserDto = require('../scripts/userDto');
 
-var userService = UserService(EventBus(), UserStorage());
-
-var firstUser = UserDto("vasya", "qwerty", "qwerty");
-var secondUser = UserDto("petya", "123456", "123456");
-var duplicatedFirstUser = UserDto("vasya", "555", "555");
-var userWithDifferentPasswords = UserDto("masha", "123", "132");
-var userWithEmptyFields = UserDto("", "", "");
-
-userService.onUserAdded(firstUser);
-userService.onUserAdded(secondUser);
-
-userService.onUserAdded(duplicatedFirstUser);
-
-userService.onUserAdded(userWithDifferentPasswords);
-
-userService.onUserAdded(userWithEmptyFields);
-
-var allusers = userService.getAll();
-
 describe("test-suite", function() {
 
 	var test = require("unit.js");
 
-	it("Should provide list of all users.", function() {
-		test
-			.object(allusers)
-			.hasLength(2)
-		;
-	});
-
 	it("Should create users.", function() {
+
+		var userService = UserService(EventBus(), UserStorage());
+		var firstUser = UserDto("vasya", "qwerty", "qwerty");
+
+		userService.onUserAdded(firstUser);
+
+		var allusers = userService.getAll();
+
 		test
 			.object(allusers)
-				.hasProperty(firstUser.username, firstUser.password)
-				.hasProperty(secondUser.username, secondUser.password)
+			.hasLength(1)
+			.hasProperty(firstUser.username, firstUser.password);
 		;
 	});
 
 	it("Should not create users with duplicate names.", function() {
+		var userService = UserService(EventBus(), UserStorage());
+		var firstUser = UserDto("vasya", "qwerty", "qwerty");
+
+		var duplicatedFirstUser = UserDto("vasya", "555", "555");
+
+		userService.onUserAdded(firstUser);
+		userService.onUserAdded(duplicatedFirstUser);
+
+		var allusers = userService.getAll();
+
 		test
 			.object(allusers)
-				.hasNotProperty(duplicatedFirstUser.username, duplicatedFirstUser.password)
+				.hasLength(1)
+				.hasNotProperty(duplicatedFirstUser.username, duplicatedFirstUser.password);
 		;
 	});
 
 	it("Should not create users with different passwords.", function() {
+		var userService = UserService(EventBus(), UserStorage());
+		var userWithDifferentPasswords = UserDto("masha", "123", "132");
+
+		userService.onUserAdded(userWithDifferentPasswords);
+
+		var allusers = userService.getAll();
+
 		test
 			.object(allusers)
+				.hasLength(0)
 				.hasNotProperty(userWithDifferentPasswords.username, userWithDifferentPasswords.password)
 		;
 	});
 
 	it("Should not create users with empty name or passwords.", function() {
+		var userService = UserService(EventBus(), UserStorage());
+		var userWithEmptyFields = UserDto("", "", "");
+
+		userService.onUserAdded(userWithEmptyFields);
+
+		var allusers = userService.getAll();
+
 		test
 			.object(allusers)
+				.hasLength(0)
 				.hasNotProperty(userWithEmptyFields.username, userWithEmptyFields.password)
 		;
 	});
